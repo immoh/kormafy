3ac3819f524c7ace8cc8912f79c9f174118f8584
Immo Heikkinen <immo.heikkinen@ekahau.com>
2014-12-18 15:24:46 +0200

    Refactor generators

--- test/kormafy/generators.clj
+++ test/kormafy/generators.clj
@@ -5,23 +5,33 @@
 (def identifier (gen/fmap (fn [[a b]] (clojure.string/join (cons a b)))
                               (gen/tuple gen/char-alpha (gen/vector gen/char-alpha-numeric))))
 
-(def aliasable (gen/fmap (fn [[name alias]] {:name name :alias alias})
-                         (gen/tuple identifier (gen/frequency [[1 (gen/return nil)] [1 identifier]]))))
+(defn prefixed-identifier [prefix]
+  (gen/fmap (partial str prefix ".") identifier))
 
-(def columns (gen/frequency [[9 (gen/not-empty (gen/vector aliasable))] [1 (gen/return [{:name "*"}])]]))
+(defn aliasable
+  ([] (aliasable nil))
+  ([prefix]
+    (gen/fmap (fn [[name alias]]
+                {:name (if prefix (str prefix "." name) name)
+                 :alias alias})
+              (gen/tuple identifier (gen/frequency [[1 (gen/return nil)] [1 identifier]])))))
+
+(defn columns [table]
+  (gen/frequency [[9 (gen/not-empty (gen/vector (aliasable table)))] [1 (gen/return [{:name (str table ".*")}])]]))
 
 (def modifier (gen/frequency [[1 (gen/return "DISTINCT")] [9 (gen/return nil)]]))
 
-(def order-by (gen/vector (gen/tuple identifier (gen/elements ["ASC" "DESC"]))))
+(defn order-by [table]
+  (gen/vector (gen/tuple (prefixed-identifier table) (gen/elements ["ASC" "DESC"]))))
 
 (def nil-or-pos-int (gen/one-of [gen/pos-int (gen/return nil)]))
 
-(defn format-columns [{table-name :name table-alias :alias} modifier columns]
+(defn format-columns [modifier columns]
   (str
     (when modifier (str modifier " "))
     (clojure.string/join ", " (map (fn [{:keys [name alias]}]
                                      (str
-                                       (or table-alias table-name) "." name
+                                       name
                                        (when alias (str " AS " alias))))
                                    columns))))
 
@@ -30,13 +40,16 @@
     (str name " AS " alias)
     name))
 
-(defn format-order-by [{:keys [name alias]} order-by-columns]
-  (clojure.string/join ", " (map (fn [[column dir]] (format "%s.%s %s" (or alias name) column dir)) order-by-columns)))
+(defn format-order-by [order-by-columns]
+  (clojure.string/join ", " (map (fn [[column dir]] (format "%s %s" column dir)) order-by-columns)))
 
 (def sql (gen/fmap (fn [[table modifier columns order-by limit offset]]
                      (str
-                       (format "SELECT %s FROM %s" (format-columns table modifier columns) (format-table table))
-                       (when (seq order-by) (str " ORDER BY " (format-order-by table order-by)))
+                       (format "SELECT %s FROM %s" (format-columns modifier columns) (format-table table))
+                       (when (seq order-by) (str " ORDER BY " (format-order-by order-by)))
                        (when limit (str " LIMIT " limit))
                        (when offset (str " OFFSET " offset))))
-                   (gen/tuple aliasable modifier columns order-by nil-or-pos-int)))
+                   (gen/bind (aliasable)
+                             (fn [{:keys [name alias] :as table}]
+                               (let [prefix (or alias name)]
+                                 (gen/tuple (gen/return table) modifier (columns prefix) (order-by prefix) nil-or-pos-int nil-or-pos-int))))))
